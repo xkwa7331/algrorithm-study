39ebbfa6980fac6c1e38f97ed6a3956a46d4f8c4
이형주 <ljhade24@hadesteve-mac.local>
2023-10-11 08:17:50 +0900

    apply coding convention(format).

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 
 
-    
 def main():
     maxdnm = 99999999
     montant = float(input("Put Amount here: "))
@@ -13,23 +12,24 @@ def main():
     for denominations in denominations:
         dy[denominations] = 1
     for i in range(1, montant + 1):
-        if dy [i] == 1:
+        if dy[i] == 1:
             continue
     count = maxdnm
     for j in range(1, i // 2 + 1):
-        if count > dy[i] + dy[i-j]:
-            count = dy[j] + dy[i-j]
+        if count > dy[i] + dy[i - j]:
+            count = dy[j] + dy[i - j]
         dy[i] = count
     print(dy[montant] if count < maxdnm else -1)
     print(dy)
     input("Press Enter to Continue")
     clear()
-    
+
+
 def clear():
-    os.system('cls')
+    os.system("cls")
     main()
+
 
 if __name__ == "__main__":
     main()
-    os.system('cls')
-
+    os.system("cls")
